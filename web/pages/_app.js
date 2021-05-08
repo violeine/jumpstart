29f5fab14a96eee0a88f5dca572d4c6b761a4dd2
violeine <haluudang@gmail.com>
2021-05-08 23:49:54 +0700

    init

---
+++ web/pages/_app.js
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+// init moai
+import "@moai/core/dist/bundle.css";
+import "@moai/core/dist/font/remote.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
